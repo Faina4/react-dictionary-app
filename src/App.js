65bc5842763ba6cd60ce957b59c90212e9edf331
FaIna <4511228@gmail.com>
2023-01-25 17:58:39 +0200

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import './App.css';
  export default function App() {
   return (
     <div className="App">
+    <div className="container">
       <header className="App-header">
         <span>   
       < img src=  {SheCodesLogo} className="App-logo img-fluid" alt="logo" /> 
 
   </span>
       </header>
+   
+    </div>
+    <footer className='test-center '>
+        Coded by
+      </footer>
     </div>
   );
 }
