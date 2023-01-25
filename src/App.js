53ddaf6bdbfe91beb1d9f1a0971c507bf47bc3b9
FaIna <4511228@gmail.com>
2023-01-25 03:16:45 +0200

    initial commit
    testing if bootstrap works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import SheCodesLogo from './SheCodesLogo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       Hello 
-        
+        <span>     
+      < img src=  {SheCodesLogo} className="App-logo img-fluid" alt="logo" /> 
+  <a href='#' className='btn btn-primary shadow'  >Hello</a>
+  </span>
       </header>
     </div>
   );
