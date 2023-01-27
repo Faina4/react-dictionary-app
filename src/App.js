e3ec3e89633f7115449f2f1683019330c0d5b62f
FaIna <4511228@gmail.com>
2023-01-28 01:50:04 +0200

    adjusted footer
    -adde github link and netlify link

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import SheCodesLogo from './SheCodesLogo.png';
 import './App.css';
 import Dictionary from'./Dictionary';
 
+
  export default function App() {
   return (
           <div className="App">
@@ -15,7 +16,8 @@ import Dictionary from'./Dictionary';
     </div>
 
          <footer >
-      <small>  Coded by</small>
+      <small>  Coded by <a href="https://github.com/Faina4/react-dictionary-app" rel="noreferrer " target="_blank" > FaIna </a> 
+      and hosted on <a href="https://sweet-dragon-0e1c62.netlify.app" rel="noreferrer " target="_blank" >Netlify </a> </small>
       </footer>
         </div>
  
