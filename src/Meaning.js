8707e17270ab7d0d25d2b0be84aa0d55c129f0dd
FaIna <4511228@gmail.com>
2023-02-05 15:00:39 +0200

    Update Meaning.js

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,31 +6,26 @@ export default function Meaning(props){
 console.log(props.meaning.definitions)
 //  <strong>Synonyms:</strong> {definition.synonyms}
 return( 
-    <div className="Meaning">
-
-
-     <h5 className="part-of-speech">
+<div className="Meaning">
+    <h5 className="part-of-speech">
      <em>  {props.meaning.partOfSpeech} </em> 
     </h5>  
     
-        {props.meaning.definitions.map(function(definition, index)
-        {
+  {props.meaning.definitions.map(function(definition, index) {
         return(       
                   
-<div key={index} className="definition">
+       <div key={index} className="definition">
     <p>
   <strong>Definition:</strong>  {definition.definition}
     <br />
   <strong>Example:</strong>  <em>  {definition.example} </em> 
    <br />
-   <Synonyms synonyms={definition.synonyms} />
+    <Synonyms synonyms={definition.synonyms} />
+     </p>
+ 
+       </div> ) 
+  })}
 
-
-    </p>
-</div>
-
-) 
- })}
-    </div>
-   
-)}
+</div> 
+)
+}
