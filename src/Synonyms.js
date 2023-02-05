7f21d3597634d0fde3b46c8bc889e961a9de69f7
FaIna <4511228@gmail.com>
2023-02-05 16:03:15 +0200

    create new branch

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,28 +2,16 @@ import React from "react";
 import "./Synonyms.css"
 
 export default function Synonyms (props){
-  //  console.log(props.synonyms)
-
-  if(props.synonyms){
+//<strong>Synonyms:</strong>
        return (
-        <div className="Synonyms">  <strong>Synonyms:</strong>  {props.synonyms.map(function(synonym, index) 
-                    
-         {
-    return( 
-       
-       
-        <ul key={index} className="synonym-list">
-           
-            <li>
-            <em>{synonym} </em> 
-            </li>
-        
-        </ul>
-         
-    )})}</div>
- 
-          )    
-  }else{
-    return null;
-}
-}
+        <div className="Synonyms"> <strong>Synonyms:</strong>
+ {props.synonyms.map(function(synonym, index) {
+            return(      
+<ul key={index} className="synonym-list">
+    <li> <em>{synonym} </em> </li>
+ </ul>
+    )})}
+        </div>     
+
+     )
+    }
