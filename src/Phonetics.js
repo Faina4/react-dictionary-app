21b12cc631244281f47655ce6ac50df62f364ceb
FaIna <4511228@gmail.com>
2023-02-06 02:03:39 +0200

    revised link
    basing on vs code recommendations
      <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen </a>

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -2,10 +2,10 @@ import React from "react";
 import "./Phonetics.css"
 
 export default function Phonetics(props){
-    console.log(props.phonetic)
+   // console.log(props.phonetic)
     return(
         <div className="Phonetics">
-          <a href={props.phonetic.audio} target="_blank"> Listen </a>   
+          <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen </a>   
       {""} {""}
           <string className="Phonetics">{props.phonetic.text}</string>     
         </div>)
