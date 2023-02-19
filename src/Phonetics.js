26b3dc66aa320a80b0fe24c2616bf090d0cb5336
FaIna <4511228@gmail.com>
2023-02-20 00:35:52 +0200

    Checked my code with SheCodes Athena
    In summary, the changes made to the code were:
    
    Added curly braces to the if statement for better readability
    Added rel="noopener noreferrer" to the link for security best practices
    Removed unnecessary spaces between elements within the span tag.

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,14 +5,12 @@ export default function Phonetics(props){
   if(props.phonetic.audio){
      return(
         <span className="Phonetics">
-          <a href={props.phonetic.audio} target="_blank" rel="noreferrer"> Listen </a>   
-      {""} {""}
+          <a href={props.phonetic.audio} target="_blank" rel="noopener noreferrer"> Listen </a>   
+      {""} 
           <span className="text">{props.phonetic.text}</span>     
         </span>)
   
   }else{
     return null;
   }
-   
-     
-}
+  }
